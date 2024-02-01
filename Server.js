54875b2b33c214c3f92c1f42362e4f40b785be1a
Rohan Raut <Rohanraut2300@gmail.com>
2024-02-01 11:36:34 +0530

    updted files

--- Server.js
+++ Server.js
@@ -1,82 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const bcrypt = require("bcryptjs");
-
-// const app = express();
-// require("./server/database/conn");
-// const Register = require("./server/models/registers");
-
-// const port = process.env.AZURE_COSMOS_CONNECTIONSTRING || 3000;
-
-// app.use(bodyParser.json())
-// app.use(express.static('public'));
-// app.use(bodyParser.urlencoded({
-//    extended:true
-// }))
-
-// app.get("/",(req,res)=> {
-//    res.render("SignUp_Page");
-// })
-
-// app.get("/login",(req,res)=> {
-//    res.render("Login_Page");
-// })
-
-// app.get("/register",(req, res)=>{
-//    res.render("SignUp_Page");
-// })
-// // app.get("/AboutUs",(req,res)=> {
-// //    res.render("AboutUs");
-// // })
-
-// // SignUp code
-// app.post("/sign_up", async(req,res)=> {
-//    try{
-//       const password = req.body.password;
-//       const ConfirmPassword = req.body.ConfirmPassword;
-//       if(password === ConfirmPassword){
-//          const registerusers = new Register({
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: password,
-//             ConfirmPassword: ConfirmPassword
-//          })
-
-//          const registered = await registerusers.save();
-//          res.status(201).sendFile(__dirname + '/public/Login_Page.html');
-         
-//       }else{
-//          res.send("Invalid Password")
-//       }
-
-//    }catch(error) {
-//       res.status(400).send(error);
-//    }
-// })
-
-// //login 
-
-// app.post("/login", async(req,res)=> {
-//    try{
-//         const email = req.body.email;
-//         const password = req.body.password;
-//         const useremail = await Register.findOne({email:email});
-
-//       const isMatch =await bcrypt.compare(password,useremail.password); 
-//         if(isMatch){
-//             res.status(201).sendFile(__dirname + '/public/index.html');
-//          }else{
-//             res.send("Invalid Password");          
-//          }    
-//    } catch(error){
-//       res.status(400).send("invalid Login Details")
-//    }
-// })
-
-// app.listen(port,()=>{
-//    console.log(`Server is running on http://localhost:${port}`)
-// });
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -158,5 +79,5 @@ app.post("/login", async (req, res) => {
 // Server listen
 app.listen(port, () => {
    //console.log(`Server is running on http://localhost:${port}`);
-   console.log("https://foodblogapp.azurewebsites.net");
+   console.log("https://foodblogpage.azurewebsites.net");
 });
